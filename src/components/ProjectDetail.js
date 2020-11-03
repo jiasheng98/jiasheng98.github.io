abdaf19e0baa030430318c4d18f5140de72a5a47
Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2020-11-03 14:23:26 +0800

    Update ProjectDetail

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -4,22 +4,24 @@ import phone from '../../public/images/iphonexmock.png';
 
 const ProjectDetail = () => {
   const PortfolioList = {
-    //   logo: snailer,
     title: 'Snailer',
-    subtitle:
-      'Took part in UI design and development for Snailer - a white-labeled food/grocery delivery and vehicle hiring app based in Johor, Malaysia. The app and website is designed using playful, clean and modern approach.',
+    projectdescription:
+      'Snailer is a mobile application and website for food/grocery delivery and vehicle hiring services.',
+    projectdescription1:
+      'There are total 3 apps and 1 website, mainly Consumer App, Rider App, Merchant App and Snailer Landing Page. The apps and website are designed using playful, clean and modern approach.',
     backgroundColor: '#22c639',
-    //   image1: snailerfood,
-    //   image2: snailergrocery,
-    //   image3: snailercar,
     landingimage: phone,
     href: '/portfolio/Snailer',
+    role: 'UI/UX Designer, UI Developer',
+    roledescription:
+      'Interaction, Visual design, Prototyping, Wireframe, Testing and Copywriting',
+    projectdate: 'July - December 2020',
   };
 
   return (
     <>
       <LandingSection item={PortfolioList} />
-      <DescriptionSection item={PortfolioList}/>
+      <DescriptionSection item={PortfolioList} />
     </>
   );
 };
@@ -35,23 +37,60 @@ const LandingSection = ({item}) => {
 };
 
 const DescriptionSection = ({item}) => {
-    return (
-      <div
-        className={styles['description-section-container']}>
-            <div
-            className={styles['description-wrapper']}
-            >
-            <h1
-            className={styles['title-text']}
-            >
-                {item.title}
-            </h1>
-            <p>
-                Overview
-            </p>
+  return (
+    <div className={styles['description-section-container']}>
+      <div className={styles['description-wrapper']}>
+        <h1 className={styles['title-text']}>{item.title}</h1>
+        <div className={styles['description-subtitle']}>
+          <div>
+            <p>Overview</p>
+            <p>{item.projectdescription}</p>
+            <p>{item.projectdescription1}</p>
+          </div>
+          <div>
+            <p>Role</p>
+            <p className={styles['role-title']}>{item.role}</p>
+            <p>{item.roledescription}</p>
+            <p>{item.projectdate}</p>
+          </div>
+        </div>
+        <div className={styles['button-container']}>
+          <a
+            style={{backgroundColor: item.backgroundColor}}
+            className={styles['prototype-button']}
+            href="/">
+            <div>
+              <p>See Prototype (Consumer)</p>
             </div>
+          </a>
+          <a
+            style={{backgroundColor: item.backgroundColor}}
+            className={styles['prototype-button']}
+            href="/">
+            <div>
+              <p>See Prototype (Rider)</p>
+            </div>
+          </a>
+          <a
+            style={{backgroundColor: item.backgroundColor}}
+            className={styles['prototype-button']}
+            href="/">
+            <div>
+              <p>See Prototype (Merchant)</p>
+            </div>
+          </a>
+          <a
+            style={{backgroundColor: item.backgroundColor}}
+            className={styles['prototype-button']}
+            href="/">
+            <div>
+              <p>See Prototype (Landing Page)</p>
+            </div>
+          </a>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ProjectDetail;

90a7422c9a0207cb4f8c4d7f75ef21c890bfc6db
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2021-08-17 02:26:40 +0800

    Update index.js

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,4 +1,4 @@
-import Layout from '../../components/layout';
+import Layout from '../../components/MyLayout';
 
 import {redirectOnAuthError, checkAuthReturnData} from '../../services/auth';
 import {isomorphicUnfetchData} from '../../services/api';

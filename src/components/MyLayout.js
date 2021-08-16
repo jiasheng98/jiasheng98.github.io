aaa135ff6fdbdceca6c95c9577bc6605b0373e64
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2021-08-17 02:44:59 +0800

    Update MyLayout.js

--- src/components/MyLayout.js
+++ src/components/MyLayout.js
@@ -1,5 +1,5 @@
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 import Head from 'next/head';
 
 export default function Layout({

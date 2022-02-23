d074656fc64b3607240059d152480e228ee20c7d
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2022-02-24 00:37:00 +0800

    seyebot

--- src/pages/seyebot/index.js
+++ src/pages/seyebot/index.js
@@ -1,68 +1,18 @@
 import {useEffect} from 'react';
 import AOS from 'aos';
+import styles from '../styles/seyebot.module.css';
 import MyLayout from '../../components/MyLayout';
-import ProjectDetail from '../../components/ProjectDetail';
-
-// Assets
-import phone from '../../../public/images/iphonexmock.png';
-import snaileruihome from '../../../public/images/snailer_ui_home.png';
 
 const SeyeBot = () => {
+
   useEffect(() => {
     AOS.init();
   });
 
-  const PortfolioList = {
-    title: 'Snailer',
-    projectdescription:
-      'Snailer is a mobile application and website for food/grocery delivery and vehicle hiring services.',
-    projectdescription1:
-      'There are total 3 apps and 1 website, mainly Consumer App, Rider App, Merchant App and Snailer Landing Page. The apps and website are designed using playful, clean and modern approach.',
-    backgroundColor: '#468d65',
-    landingimage: snaileruihome,
-    role: 'UI/UX Designer, UI Developer',
-    roledescription:
-      'Interaction, Visual design, Prototyping, Wireframe, Testing and Copywriting',
-    projectdate: 'July - December 2020',
-    appstorelink: 'https://apps.apple.com/us/app/snailer/id1540536548',
-    playstorelink:
-      'https://play.google.com/store/apps/details?id=com.snailer_consumer&hl=en&gl=US',
-    prototype: false,
-    appstore: true,
-    playstore: true,
-    designsoftware: 'Adobe XD, Adobe Photoshop, Adobe Illustrator, Adobe After Effects',
-    developmentsoftware: 'React, HTML, CSS, Javascript',
-  };
-
-  const Data = [
-    {
-      label: 'Consumer',
-      href: '/index',
-    },
-    {
-      label: 'Merchant',
-      href: '/index',
-    },
-    {
-      label: 'Rider',
-      href: '/index',
-    },
-    {
-      label: 'Landing Page',
-      href: '/index',
-    },
-  ];
-
-  const Website = [
-    {
-      label: 'Landing Page',
-      href: 'http://thesnailer.com/',
-    },
-  ];
 
   return (
-    <MyLayout title="Snailer">
-      <ProjectDetail item={PortfolioList} data={Data} website={Website} />
+    <MyLayout title="SeyeBot">
+
     </MyLayout>
   );
 };

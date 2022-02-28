4f72a9f6e8b2b3f39815252b9f97def29bba67dd
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2022-02-28 23:09:44 +0800

    edit

--- src/components/seyebot/ci-calculator.js
+++ src/components/seyebot/ci-calculator.js
@@ -1,22 +1,22 @@
-import * as React from "react";
+import * as React from 'react';
 
-import VariablesForm from "../../components/seyebot/variables-form";
-import CompoundInterestChart from "../../components/seyebot/ci-chart";
+import VariablesForm from '../../components/seyebot/variables-form';
+import CompoundInterestChart from '../../components/seyebot/ci-chart';
 
-import defaultState from "../../components/seyebot/default-state";
+import defaultState from '../../components/seyebot/default-state';
 import styles from '../styles/ci-calculator.module.css';
 
 const CICalculator = () => {
   const [state, setState] = React.useState(defaultState);
 
   return (
-    <div
-    className={styles['ci-container']}
-    id="annualCompoundCalculator">
-      <h1 data-aos="fade-up" className={styles['text-center']}>Calculate How Much You Can Earn!</h1>
-      <hr className={styles['hr']}/>
+    <div className={styles['ci-container']} id="annualCompoundCalculator">
+      <h1 data-aos="fade-up" className={styles['text-center']}>
+        Calculate How Much You Can Earn!
+      </h1>
+      <hr className={styles['hr']} />
       <VariablesForm onUpdate={variables => setState(variables)} />
-      <hr className={styles['hr']}/>
+      <hr className={styles['hr']} />
       <CompoundInterestChart {...state} />
     </div>
   );
--- src/pages/seyebot/index.js
+++ src/pages/seyebot/index.js
@@ -1,31 +1,84 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import AOS from 'aos';
 import styles from '../styles/seyebot.module.css';
 import MyLayout from '../../components/MyLayout';
 import Contact from '../../components/home/Contact';
 import CICalculator from '../../components/seyebot/ci-calculator';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import {CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import Seye from '../../../public/images/seye-logo.png';
+import Typed from 'typed.js';
 
 const SeyeBot = () => {
   useEffect(() => {
     AOS.init();
   });
 
+  useEffect(() => {
+    const options = {
+      strings: ['Bot', 'AI'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    };
+
+    const typed = new Typed(el, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  let el = useRef();
 
   return (
     <MyLayout title="SeyeBot">
-      <div
-      className={styles['seye-bot-container']}
-      >
-<h1>Seye-Bot</h1>
-<p>An Expert Advisor </p>
-<div style={{ width: 200, height: 200 }}>
-  <CircularProgressbar value={66} text={`85%`} />
-</div>
+      <div className={styles['seye-bot-container']}>
+        <div className={styles['seye-bot-wrapper']}>
+          <img src={Seye} className={styles['seye-logo']} />
+          <h1>Seye-<span ref={(e) => (el = e)}/></h1>
+          <p>Automate Your Trading Experience Like No Other</p>
+          <svg
+            className={styles['pulse-svg']}
+            version="1.2"
+            height="300"
+            width="600"
+            xmlns="http://www.w3.org/2000/svg"
+            viewport="0 0 60 60"
+            xmlnsXlink="http://www.w3.org/1999/xlink">
+            <path
+              stroke="rgba(15,0,0,0.1)"
+              fill="none"
+              stroke-width="1"
+              stroke-linejoin="round"
+              d="
+            M0,90L250,90Q257,60 262,87T267,95 270,88 273,92t6,35 7,-60T290,127 297,107s2,-11 10,-10 1,1 8,-10T319,95c6,4 8,-6 10,-17s2,10 9,11h210"
+            />
+            <path
+              className={styles['pulsar']}
+              stroke="rgba(0,155,155,1)"
+              fill="none"
+              stroke-width="1"
+              stroke-linejoin="round"
+              d="
+            M0,90L250,90Q257,60 262,87T267,95 270,88 273,92t6,35 7,-60T290,127 297,107s2,-11 10,-10 1,1 8,-10T319,95c6,4 8,-6 10,-17s2,10 9,11h210"
+            />
+          </svg>
+        </div>
+        {/* <div
+        className={styles['progress-bar-container']}
+        >
+        <div
+        className={styles['progress-bar']}
+        >
+          <CircularProgressbar value={66} text={`85%`} />
+        </div>
+        <h1>Annual Percentage Yield (APY)</h1>
+        </div> */}
+        <div>hi</div>
       </div>
-         <div className={styles['seye-bot-graph-container']}>
-         <CICalculator/>
+      <div className={styles['seye-bot-graph-container']}>
+        <CICalculator />
       </div>
       <Contact />
     </MyLayout>

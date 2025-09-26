a711650685aadf4ff7a168923ad880567d2132b9
Jia Sheng | シェーン <66236001+jiasheng98@users.noreply.github.com>
2025-09-27 01:23:59 +0900

    Consolidate CSS into a single stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
-import '../components/styles/header.css';
-import '../components/styles/footer.css';
-import '../components/styles/about.css';
-import '../components/styles/contact.css';
-import '../components/styles/landing-section.css';
-import '../components/styles/work-section.css';
-import '../components/styles/project-detail.css';
-import '../pages/styles/portfolio-list.css';
+import '../styles/style.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
